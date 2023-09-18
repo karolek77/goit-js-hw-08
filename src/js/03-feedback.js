78bd0416ab7bb996cf7b3acd1ca7307a9df03c9c
karolek77 <karoolkoska@gmail.com>
2023-09-18 08:24:55 +0200

    obiekt + consol.log w feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,47 @@
 import throttle from 'lodash.throttle';
 
-  const form = document.querySelector('.feedback-form');
-  const emailInput = form.querySelector("input[name='email']");
-  const messageTextarea = form.querySelector("textarea[name='message']");
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector("input[name='email']");
+const messageTextarea = form.querySelector("textarea[name='message']");
 
-  const localStorageKey = 'feedback-form-state';
-  form.addEventListener('input', () => {
-    const formData = {
+const localStorageKey = 'feedback-form-state';
+form.addEventListener('input', () => {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+try {
+  const savedSettings = localStorage.getItem(localStorageKey);
+  const parsedSetings = JSON.parse(savedSettings);
+  if (parsedSetings && parsedSetings.email && parsedSetings.message) {
+    emailInput.value = parsedSetings.email;
+    messageTextarea.value = parsedSetings.message;
+  }
+} catch (error) {
+  console.error('Błąd podczas odczytu danych z localStorage: ', error.message);
+}
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const dataSave = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(dataSave);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const dataToSave = {
       email: emailInput.value,
       message: messageTextarea.value,
     };
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-  });
-
-  try {
-    const savedSettings = localStorage.getItem(localStorageKey);
-    const parsedSetings = JSON.parse(savedSettings);
-    if (parsedSetings && parsedSetings.email && parsedSetings.message) {
-      emailInput.value = parsedSetings.email;
-      messageTextarea.value = parsedSetings.message;
-    }
-  } catch (error) {
-    console.error(
-      'Błąd podczas odczytu danych z localStorage: ',
-      error.message
-    );
-  }
-
-  form.addEventListener('submit', evt => {
-    evt.preventDefault();
-    localStorage.removeItem(localStorageKey);
-    form.reset();
-  });
-
-  form.addEventListener(
-    'input',
-    throttle(() => {
-      const dataToSave = {
-        email: emailInput.value,
-        message: messageTextarea.value,
-      };
-      localStorage.setItem(localStorageKey, JSON.stringify(dataToSave));
-    }, 500)
-  );
-
+    localStorage.setItem(localStorageKey, JSON.stringify(dataToSave));
+  }, 500)
+);
